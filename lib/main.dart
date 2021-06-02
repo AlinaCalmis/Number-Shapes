e196fd6e57d1d9505ece6d55fdad343df616ead1
AlinaCalmis <alinacalmis1@gmail.com>
2021-06-02 13:03:37 +0300

    analysis

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:powers/powers.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: NumberShapes(),
-      )
-  );
+  runApp(const MaterialApp(
+    home: NumberShapes(),
+  ));
 }
 
 class NumberShapes extends StatefulWidget {
-  const NumberShapes({Key key}) : super(key: key);
+  const NumberShapes({Key? key}) : super(key: key);
 
   @override
   _NumberShapesState createState() => _NumberShapesState();
 }
 
 class _NumberShapesState extends State<NumberShapes> {
-  TextEditingController _controller = new TextEditingController();
+  final TextEditingController _controller = TextEditingController();
+  final TextInputType _textInputType = TextInputType.number;
   int _readNumber = 0;
 
   // generates Dialog Alert pop up window
   void getAlert(BuildContext context, String text) {
     if (_controller.text.isNotEmpty) {
-      showDialog(
+      showDialog<Widget>(
           context: context,
-          builder: (context) {
+          builder: (BuildContext context) {
             return AlertDialog(
-                title: Text("$_readNumber"),
-                content: Text("Number $_readNumber $text"),
-              );
-          }
-      );
+              title: Text('$_readNumber'),
+              content: Text('Number $_readNumber $text'),
+            );
+          });
     }
   }
 
@@ -39,19 +37,18 @@ class _NumberShapesState extends State<NumberShapes> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center (
-        child: Text("Number Shapes"),
-        )
-      ),
+          title: const Center(
+        child: Text('Number Shapes'),
+      )),
       body: Column(
         children: <Widget>[
-          new Text("Please input a number to see if it is square or triangular",
-          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20), textAlign: TextAlign.center),
-          new Padding (
-            padding: EdgeInsets.all(10),
-              child: TextField(
+          const Text('Please input a number to see if it is square or triangular',
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20), textAlign: TextAlign.center),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
               controller: _controller,
-              keyboardType: TextInputType.numberWithOptions(signed: false, decimal: false),
+              keyboardType: _textInputType,
             ),
           )
         ],
@@ -59,22 +56,20 @@ class _NumberShapesState extends State<NumberShapes> {
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             setState(() {
-
-              _readNumber = int.tryParse(_controller.text);
+              _readNumber = int.tryParse(_controller.text)!;
 
               if (_readNumber != null) {
                 if (_readNumber.isSquare) {
-                  getAlert(context, " is SQUARE");
+                  getAlert(context, ' is SQUARE');
                 } else if (_readNumber.isCube) {
-                  getAlert(context, " is TRIANGULAR");
+                  getAlert(context, ' is TRIANGULAR');
                 } else {
-                  getAlert(context, " is neither TRIANGULAR nor SQUARE");
+                  getAlert(context, ' is neither TRIANGULAR nor SQUARE');
                 }
               }
             });
           },
-          child: const Icon(Icons.check)
-      ),
+          child: const Icon(Icons.check)),
     );
   }
 }
